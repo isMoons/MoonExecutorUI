52b4c572bd3d9b3ae4d7c60435522104f9d54e29
isMoon <naelprakasa@gmail.com>
2025-01-12 16:18:42 +0700

    Update Example.client.lua

--- MoonExecutor/MoonUIV3-Fluent/Fluent-master/Example.client.lua
+++ MoonExecutor/MoonUIV3-Fluent/Fluent-master/Example.client.lua
@@ -224,30 +224,6 @@ Tabs.AutoFarm:AddButton({
         })
     end
 })
-Tabs.AutoFarm:AddButton({
-    Title = "TestButtonV2",
-    Description = "Test",
-    Callback = function()
-        Window:Dialog({
-            Title = "Title",
-            Content = "This is a dialog",
-            Buttons = {
-                {
-                    Title = "Confirm",
-                    Callback = function()
-                        print("Confirmed the dialog.")
-                    end
-                },
-                {
-                    Title = "Cancel",
-                    Callback = function()
-                        print("Cancelled the dialog.")
-                    end
-                }
-            }
-        })
-    end
-})
 -- // NEW CODE TEST //
 Main:Notify({
     Title = "Fluent",
